b123b92a3cdb012a5e0891988d80dd6bb03fc03d
ALindroos <adiel.lindroos@helsinki.fi>
2019-01-20 21:37:34 +0200

    edit function for books

--- application/books/views.py
+++ application/books/views.py
@@ -11,10 +11,25 @@ def books_index():
 def books_form():
     return render_template("books/new.html")
 
+@app.route("/books/edit/<book_id>/", methods=["POST"])
+def books_edit(book_id):
+    return render_template("books/edit.html", book = Book.query.get(book_id))
+
+@app.route("/books/upd/<book_id>", methods=["POST"])
+def books_update(book_id):
+
+    b = Book.query.get(book_id)
+    b.title = request.form.get("title")
+    b.author = request.form.get("author")
+    db.session().commit()
+
+    return redirect(url_for("books_index"))
+
+
 @app.route("/books/", methods=["POST"])
 def books_create():
 
-    b = Book("book","author")
+    b = Book(request.form.get("title"),request.form.get("author"))
 
     db.session().add(b)
     db.session().commit()
