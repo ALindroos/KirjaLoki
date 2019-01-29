37113ac69c13cfa68684881ab3259f0ba6eebdb5
ALindroos <adiel.lindroos@helsinki.fi>
2019-01-29 13:18:14 +0200

    logout func, login required paths for important functionality

--- application/__init__.py
+++ application/__init__.py
@@ -8,6 +8,7 @@ app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
+#Sovelluksen toiminallisuus
 from application import views
 
 from application.books import models
@@ -16,5 +17,21 @@ from application.books import views
 from application.auth import models
 from application.auth import views
 
+#Kirjautuminen
+from application.auth.models import User
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
 
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "Kirjaudu sisään"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+#Create database
 db.create_all()
--- application/books/views.py
+++ application/books/views.py
@@ -1,24 +1,32 @@
-from application import app, db
 from flask import render_template, request, redirect, url_for
+from flask_login import login_required
+
+from application import app, db
 from application.books.models import Book
 from application.books.forms import BookForm
 
-
+#Basic view, shows all books
 @app.route("/books", methods=["GET"])
 def books_index():
     return render_template("books/list.html", books = Book.query.all())
 
+#Create new book form
 @app.route("/books/new/")
+@login_required
 def books_form():
     return render_template("books/new.html", form = BookForm())
 
+#Edit book
 @app.route("/books/edit/<book_id>", methods=["GET"])
+@login_required
 def books_edit(book_id):
     form = BookForm(request.form)
     form.description.data = Book.query.get(book_id).description
     return render_template("books/edit.html", book = Book.query.get(book_id), form = form)
 
+#Update book data
 @app.route("/books/upd/<book_id>", methods=["POST"])
+@login_required
 def books_update(book_id):
     form = BookForm(request.form)
 
@@ -30,13 +38,19 @@ def books_update(book_id):
 
     return redirect(url_for('book_show', book_id=book_id))
 
+#Show individual book
 @app.route("/books/<book_id>", methods=["GET"])
 def book_show(book_id):
     return render_template("books/book.html", book = Book.query.get(book_id))
 
+#Create new book
 @app.route("/books/", methods=["POST"])
+@login_required
 def books_create():
     form = BookForm(request.form)
+
+    #add validations
+    #....
 
     b = Book(form.title.data, form.author.data, form.description.data)
 
@@ -45,11 +59,12 @@ def books_create():
 
     return redirect(url_for("books_index"))
 
+#Search books, matches author&title
 @app.route("/books/seach/")
 def books_search():
     return render_template("books/search.html")
 
-
+#Show search results
 @app.route("/books/results/", methods=["POST"])
 def books_results():
     st = request.form.get("seachTerm")
